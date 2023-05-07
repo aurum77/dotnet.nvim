f9439439747e66cc4bfd910fd9588f571a11ee44
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-05-07 11:17:26 +0300

    fix: add some checks for filetypes in template generation

--- lua/dotnet/nvim-tree.lua
+++ lua/dotnet/nvim-tree.lua
@@ -7,9 +7,13 @@ local Event = nvim_tree_api.events.Event
 function M.setup_nvim_tree()
   nvim_tree_api.events.subscribe(Event.FileCreated, function(data)
     local file_path = data.fname
-    local file_name = file_path:match("[^/]*.cs$"):gsub(".cs", "")
     local file_type
+    local file_name
     local namespace = utils.get_node_namespace()
+
+    if file_path:match "[^/]*.cs$" ~= nil then
+      file_name = file_path:match("[^/]*.cs$"):gsub(".cs", "")
+    end
 
     if file_path:sub(-3) == ".cs" then
       if file_name:sub(1, 1) == "I" and file_name:sub(1, 2) == file_name:sub(1, 2):upper() then
@@ -24,24 +28,24 @@ function M.setup_nvim_tree()
       -- Class file
       io.write(
         "using System;\n"
-        .. "using System.Collections.Generic;\n"
-        .. "using System.Linq;\n"
-        .. "using System.Text;"
-        .. "\n"
-        .. "using System.Threading.Tasks;\n\n"
-        .. "namespace "
-        .. namespace
-        .. "\n"
-        .. "{\n"
-        .. "    public "
-        .. file_type
-        .. " "
-        .. file_name
-        .. "\n"
-        .. "    {\n"
-        .. "        \n"
-        .. "    }\n"
-        .. "}"
+          .. "using System.Collections.Generic;\n"
+          .. "using System.Linq;\n"
+          .. "using System.Text;"
+          .. "\n"
+          .. "using System.Threading.Tasks;\n\n"
+          .. "namespace "
+          .. namespace
+          .. "\n"
+          .. "{\n"
+          .. "    public "
+          .. file_type
+          .. " "
+          .. file_name
+          .. "\n"
+          .. "    {\n"
+          .. "        \n"
+          .. "    }\n"
+          .. "}"
       )
       io.close(cs_file)
     end
