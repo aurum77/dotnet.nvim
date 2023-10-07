eb926c645ba295ce7eaaeed70289e3d146322eb2
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-10-07 20:34:26 +0300

    remove start function and use setup instead

--- lua/dotnet.lua
+++ lua/dotnet.lua
@@ -13,15 +13,10 @@ if vim.g.loaded_dotnet == 1 then
   return
 end
 
-function M.start()
+function M.setup()
   nvim_tree.setup_nvim_tree()
   command("DotnetProject", commands.dotnet_project)
   command("DotnetManage", commands.dotnet_manage)
-end
-
-function M.setup()
-  vim.g.loaded_dotnet = 1
-  autocmds.register_autocmds()
 end
 
 return M
