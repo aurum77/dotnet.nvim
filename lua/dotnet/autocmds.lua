084e1a99186783828aa9892c3a537828d1784fd1
aurum77 <58880138+aurum77@users.noreply.github.com>
2023-10-07 18:35:22 +0300

    remove commented code

--- lua/dotnet/autocmds.lua
+++ lua/dotnet/autocmds.lua
@@ -1,19 +1,5 @@
 local M = {}
 
--- function M.register_autocmds()
---   -- Disable non-standard semantic tokens coming from omnisharp lsp
---   vim.api.nvim_create_autocmd("LspAttach", {
---     desc = "Fix startup error by disabling semantic tokens for omnisharp",
---     group = vim.api.nvim_create_augroup("OmnisharpHook", {}),
---     callback = function(ev)
---       local client = vim.lsp.get_client_by_id(ev.data.client_id)
---       if client.name == "omnisharp" then
---         client.server_capabilities.semanticTokensProvider = nil
---       end
---     end,
---   })
--- end
-
 function M.register_autocmds()
   local dotnet_group = vim.api.nvim_create_augroup("DotnetGroup", {})
   local dotnet = require "dotnet"
@@ -24,18 +10,6 @@ function M.register_autocmds()
     once = true,
     callback = dotnet.start,
   })
-
-  -- Disable non-standard semantic tokens coming from omnisharp lsp
-  -- vim.api.nvim_create_autocmd("LspAttach", {
-  --   desc = "Fix startup error by disabling semantic tokens for omnisharp",
-  --   group = dotnet_group,
-  --   callback = function(ev)
-  --     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-  --     if client.name == "omnisharp" then
-  --       client.server_capabilities.semanticTokensProvider = nil
-  --     end
-  --   end,
-  -- })
 end
 
 return M
